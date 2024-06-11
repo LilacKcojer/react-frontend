a78468ddb245a549274184aed3daecfcc73a52c6
Lilac Kcojer <lilackcojer@gmail.com>
2024-06-10 22:40:25 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Amplify } from 'aws-amplify';
 
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import Home from "./components/Home";
+
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Home({user, signOut})}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+
+const Home = ({user, signOut}) => {
+
+    const [users, setUsers] = useState(null);
+    
+    const requestBody = {
+        email: 'email2@test.com'
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+    };
+
+    const getUsers = async () => {
+        const response = await fetch("https://huix8w2yq5.execute-api.us-west-2.amazonaws.com/prod/users", options)
+        const data = await response.json();
+
+        setUsers(data);
+    
+    }
+    
+    useEffect(() => {
+        getUsers();
+    },[]);
+
+    return (
+        <>
+            <h1>Hello {user.username}</h1>
+            <h1>{users?.email}</h1>
+            <button onClick={signOut}>Sign out</button>
+        </>
+    )
+};
+
+export default Home;
