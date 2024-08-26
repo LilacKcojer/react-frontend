596d21f75dd2c41595998f26aa89d259826ff4f5
Lilac Kcojer <lilackcojer@gmail.com>
2024-08-26 18:24:15 -0400

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,20 +24,6 @@ const Home = ({user, signOut}) => {
             <ListItem key={text} disablePadding>
                 <ListItemButton>
                 <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-                </ListItemButton>
-            </ListItem>
-            ))}
-        </List>
-        <Divider />
-        <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-                <ListItemButton>
-                <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
                 <ListItemText primary={text} />
                 </ListItemButton>
