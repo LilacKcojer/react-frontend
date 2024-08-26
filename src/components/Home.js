9b1617348959ab8713bb58d95bc5e6fb7924e76c
Lilac Kcojer <lilackcojer@gmail.com>
2024-08-26 18:06:36 -0400

    added drawer component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,52 @@
 import React, {useState, useEffect} from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 const Home = ({user, signOut}) => {
+
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
+
+    const DrawerList = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+        <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+                <ListItemButton>
+                <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+                </ListItemButton>
+            </ListItem>
+            ))}
+        </List>
+        <Divider />
+        <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+                <ListItemButton>
+                <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+                </ListItemButton>
+            </ListItem>
+            ))}
+        </List>
+        </Box>
+    );
+
 
     const [users, setUsers] = useState(null);
     
@@ -30,8 +76,11 @@ const Home = ({user, signOut}) => {
 
     return (
         <>
-            <h1>Hello {user.username}</h1>
             <h1>{users?.email}</h1>
+            <Button onClick={toggleDrawer(true)}>Pages</Button>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+            {DrawerList}
+            </Drawer>
             <button onClick={signOut}>Sign out</button>
         </>
     )
